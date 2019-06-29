e2cbdd780c95c2b2757dca1ff4a554fd6c98fba5
Nick Heise <nick@heise.org>
2019-06-29 17:01:00 +0200

    add reg term and refactoring

--- nets.py
+++ nets.py
@@ -2,12 +2,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class MappingNet(nn.Module):
 
     def __init__(self):
         super(MappingNet, self).__init__()
         self.fc1 = nn.Linear(in_features=2, out_features=2)
-
 
     def forward(self, x):
         x = self.fc1(x)
@@ -19,11 +19,14 @@ class DiscriminatorNet(nn.Module):
     def __init__(self):
         super(DiscriminatorNet, self).__init__()
         self.fc1 = nn.Linear(in_features=2, out_features=8)
-        self.fc2 = nn.Linear(in_features=8, out_features=1)
-
+        self.fc2 = nn.Linear(in_features=8, out_features=8)
+        self.fc3 = nn.Linear(in_features=8, out_features=1)
 
     def forward(self, x):
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+
         return torch.sigmoid(x)
